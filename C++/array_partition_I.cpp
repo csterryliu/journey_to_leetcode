74becb3c0c3b2934d9e19741683dcd2d17a830b6
Terry_Liu <tkuterry.liu@gmail.com>
2018-11-03 15:08:46 +0800

    c++: array partition I

---
+++ C++/array_partition_I.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "./array_partition_I.h"
+#include "./utility.h"
+
+using namespace std;
+
+int Solution::arrayPairSum(vector<int>& nums) {
+    // sort the array first
+    // nlog(n)
+    sort(nums.begin(), nums.end());
+    Utility::printVector(nums);
+    int output = 0;
+    // O(n/2) -> O(n)
+    for (int i = 0; i < nums.size(); i+=2) {
+        cout << nums[i] << nums[i+1] << "\n";
+        output += min(nums[i], nums[i+1]);
+    }
+    // total: O(nlogn + n)
+    return output;
+}
+
+int main() {
+    vector<int> input;
+    input.push_back(1);
+    input.push_back(1);
+    Utility::printVector(input);
+
+    Solution* sol = new Solution();
+    int output = sol->arrayPairSum(input);
+    delete sol;
+
+    cout << output << "\n";
+
+    return 0;
+}
---
+++ C++/array_partition_I.h
@@ -0,0 +1,9 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution() {}
+    int arrayPairSum(vector<int>& nums);
+};
