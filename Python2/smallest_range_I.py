867b146b5c29da84b8a76a0b5841b1bab790ade0
Terry_Liu <tkuterry.liu@gmail.com>
2018-11-07 07:02:25 +0800

    py: smallest range I

---
+++ Python2/smallest_range_I.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        """
+        from leetcode solution:
+        
+        Let A be the original array, and B be the array after all our modifications. Towards trying to minimize max(B) - min(B), let's try to minimize max(B) and maximize min(B) separately.
+
+        The smallest possible value of max(B) is max(A) - K, as the value max(A) cannot go lower. Similarly, the largest possible value of min(B) is min(A) + K. So the quantity max(B) - min(B) is at least ans = (max(A) - K) - (min(A) + K).
+        """
+        return max(0, max(A) - min(A) - 2*K)
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.smallestRangeI([1], 0)
