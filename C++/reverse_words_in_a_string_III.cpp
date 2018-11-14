82b0a1d5a92f472e2b6a44abb1cb7c8cf088e01a
Terry_Liu <tkuterry.liu@gmail.com>
2018-11-14 23:18:36 +0800

    C++: reverse words in a string III

--- C++/reverse_words_in_a_string_III.cpp
+++ C++/reverse_words_in_a_string_III.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-#include <map>  // map is in sorted order
 #include "./reverse_words_in_a_string_III.h"
-#include "./utility.h"
 
 using namespace std;
 
 string Solution::reverseWords(string s) {
-    string output = "";
-    return output;
+    if (s.length() == 1) return s;
+    string::iterator iter;
+    string::iterator temp_start_iter = s.begin();
+    for (iter = s.begin(); iter != s.end(); ++iter) {
+        if (*iter == ' ') {
+            reverse(temp_start_iter, iter);
+            temp_start_iter = iter+1;
+        }
+    }
+    reverse(temp_start_iter, s.end());
+
+    // the time complexity may be O(N + N) => O(N)
+    return s;
 }
 
 int main() {
