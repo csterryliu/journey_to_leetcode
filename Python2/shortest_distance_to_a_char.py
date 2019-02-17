54a50f1b47832347871eba28047c9752e5d4f4cc
Terry_Liu <tkuterry.liu@gmail.com>
2019-02-17 17:52:00 +0800

    py: shortest distance to a char

---
+++ Python2/shortest_distance_to_a_char.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def shortestToChar(self, S, C):
+        output = []
+        distance_pre = 0
+        distance_post = 0
+        for i in xrange(len(S)):
+            if S[i] == C:
+                output.append(0)
+                continue
+            # search backword
+            temp = i - len(S)
+            while temp >= -len(S):
+                if S[temp] == C:
+                    break
+                distance_pre += 1
+                temp -= 1
+            if temp < -len(S):
+                distance_pre = 0xFFFFFFFF  # not found
+            # search forword
+            temp = i
+            while temp < len(S):
+                if S[temp] == C:
+                    break
+                distance_post += 1
+                temp += 1
+            if temp == len(S):
+                distance_post = 0xFFFFFFFF  # not found
+            # append the shortest distance
+            output.append(min(distance_pre, distance_post))
+            distance_pre = 0
+            distance_post = 0
+
+        return output
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.shortestToChar('abaa', 'b')
