02dde9748c39011fd73c5a13624b754047d08083
Terry Liu <csterryliu@users.noreply.github.com>
2019-03-01 18:56:19 +0800

    Add binary search solution

--- Python2/two_sum_sorted_array.py
+++ Python2/two_sum_sorted_array.py
@@ -5,15 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-		  r = 0
-   		l = len(numbers) - 1
-		  # O(n)
-		  while r < l:
-	  	  s = numbers[r] + numbers[l]
-			  if s > target:
-	  		    l -= 1
-	  		elif s < target:
-	  		    r += 1
-		  	else:
-		  		return [r+1, l+1]
-	  	return None
+        r = 0
+   	l = len(numbers) - 1
+	# O(n)
+	while r < l:
+	    s = numbers[r] + numbers[l]
+	    if s > target:
+	        l -= 1
+	    elif s < target:
+	  	r += 1
+	    else:
+		return [r+1, l+1]
+	return None
+	
+    def twoSumBinarySearch(self, numbers, target):
+	#O(nlogn)
+	for i in range(len(numbers)):
+	    l = i + 1
+	    r = len(numbers) - 1
+	    compliment =  target - numbers[i]
+            # using binary search to find the compliment
+	    while l <= r:
+	        mid = l + ((r - l) / 2). # or (l + r)/2
+	        if numbers[mid] == compliment:
+		    return [i+1, mid+1]
+	        if numbers[mid] > compliment:
+	            r = mid - 1
+	        elif numbers[mid] < compliment:
+	            l = mid + 1
+	return None
