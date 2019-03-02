617b8a51dd9a3abebab050df7504a02af0d4563f
Terry_Liu <tkuterry.liu@gmail.com>
2019-03-02 10:24:33 +0800

    Fix messy code

--- Python2/two_sum_sorted_array.py
+++ Python2/two_sum_sorted_array.py
@@ -5,32 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        r = 0
-   	l = len(numbers) - 1
-	# O(n)
-	while r < l:
-	    s = numbers[r] + numbers[l]
-	    if s > target:
-	        l -= 1
-	    elif s < target:
-	  	r += 1
-	    else:
-		return [r+1, l+1]
-	return None
-	
+        l = 0
+        r = len(numbers) - 1
+        # O(n)
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s > target:
+                r -= 1
+            elif s < target:
+                l += 1
+            else:
+                return [l+1, r+1]
+        return None
+
     def twoSumBinarySearch(self, numbers, target):
-	#O(nlogn)
-	for i in range(len(numbers)):
-	    l = i + 1
-	    r = len(numbers) - 1
-	    compliment =  target - numbers[i]
+        # O(nlogn)
+        for i in range(len(numbers)):
+            l = i + 1
+            r = len(numbers) - 1
+            compliment = target - numbers[i]
             # using binary search to find the compliment
-	    while l <= r:
-	        mid = l + ((r - l) / 2). # or (l + r)/2
-	        if numbers[mid] == compliment:
-		    return [i+1, mid+1]
-	        if numbers[mid] > compliment:
-	            r = mid - 1
-	        elif numbers[mid] < compliment:
-	            l = mid + 1
-	return None
+            while l <= r:
+                mid = l + ((r - l) / 2)  # or (l + r)/2
+                if numbers[mid] == compliment:
+                    return [i+1, mid+1]
+                if numbers[mid] > compliment:
+                    r = mid - 1
+                elif numbers[mid] < compliment:
+                    l = mid + 1
+        return None
