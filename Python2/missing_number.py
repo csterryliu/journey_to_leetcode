69c6924b1cd4de29129eec2d72aff71ceada9b92
Terry_Liu <tkuterry.liu@gmail.com>
2019-07-10 07:24:46 +0800

    fix code

--- Python2/missing_number.py
+++ Python2/missing_number.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def missingNumber(self, nums):
         s = set(nums)
-        # because the array must miss one number, so n is len(nums) + 1
-        n = len(nums) + 1
+        # because the array must miss one number
+        expectedNumCount = len(nums) + 1
         # use the set to find the missing number
-        for number in xrange(n):
+        for number in xrange(expectedNumCount):
             if number not in s:
                 return number
         # time complexity overall: O(n) of set creation + O(n) of loop => O(n)
