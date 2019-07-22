8641aa9c9ae22cfd187963fc0bd42db974c13ef5
Terry_Liu <tkuterry.liu@gmail.com>
2019-07-23 06:33:49 +0800

    backup

---
+++ Python2/house_robber.py
@@ -0,0 +1,14 @@
+class Solution(object):
+
+
+    # state: maxMoney(n) => The maximum amount of money you can rob by robbing n houses at one night without alerting the police
+    def rob(self, nums):
+        def recursive(nums, n):
+            
+
+
+        return 0
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.rob([1,2,3,1])
